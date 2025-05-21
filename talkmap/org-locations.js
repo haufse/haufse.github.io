c572347368f7ac14af73d81dc40996734823ed09
haufse <erik@haufs.de>
2025-05-21 09:01:06 +0200

    update map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,40 +1,23 @@
+
 var addressPoints = [
   [
-    "HHU, Düsseldorf, Germany",
-    51.187,
-    6.798
-  ]
-  [
-    "RUB, Bochum, Germany",
-    51.445412417117566, 
-    7.263424735529526
+    "Berkeley CA, USA",
+    37.8708393,
+    -122.2728638
   ],
   [
-    "UNC, Chapel Hill, NC",
-    35.905,
-    -79.047
+    "London, UK",
+    51.5073219,
+    -0.1276473
+  ],
+  [
+    "San Francisco, California",
+    37.7792808,
+    -122.4192362
+  ],
+  [
+    "Los Angeles, CA",
+    34.0543942,
+    -118.2439408
   ]
 ];
-
-// var addressPoints = [
-//   [
-//     "Berkeley CA, USA",
-//     37.8708393,
-//     -122.2728638
-//   ],
-//   [
-//     "London, UK",
-//     51.5073219,
-//     -0.1276473
-//   ],
-//   [
-//     "San Francisco, California",
-//     37.7792808,
-//     -122.4192362
-//   ],
-//   [
-//     "Los Angeles, CA",
-//     34.0543942,
-//     -118.2439408
-//   ]
-// ];
