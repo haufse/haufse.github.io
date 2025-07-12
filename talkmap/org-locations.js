90fdd0104dd6c31e7b14fc3917c2e8610fe110c4
haufse <erik@haufs.de>
2025-07-12 23:17:01 +0200

    manually run talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,8 +1,33 @@
 var addressPoints = [
   [
+    "Auf dem H\u00fcgel 20, 53121 Bonn, Germany",
+    50.7311926,
+    7.0707041
+  ],
+  [
+    "405 Rue Sainte-Catherine Est, Montr\u00e9al, QC H2L 2C4",
+    45.513713,
+    -73.5617177
+  ],
+  [
+    "Ludwigstra\u00dfe 23, 35390 Gie\u00dfen, Germany",
+    50.5804148,
+    8.6771688
+  ],
+  [
+    "Vogelpothsweg 87, 44227 Dortmund, Germany",
+    51.4920599,
+    7.4153013
+  ],
+  [
     "Ruhr University Bochum, Universit\u00e4tsstra\u00dfe 150, 44801 Bochum, Germany",
     51.4441576,
     7.2609654
+  ],
+  [
+    "700 University Ave, Toronto, ON M5G 1Z5",
+    43.6591104,
+    -79.3912066
   ],
   [
     "University of North Carolina at Chapel Hill, Chapel Hill, NC, USA",
@@ -11,7 +36,7 @@ var addressPoints = [
   ],
   [
     "Universit\u00e4tsstra\u00dfe 1, 40225 D\u00fcsseldorf, Germany",
-    51.1848493,
-    6.8000157
+    51.1907732,
+    6.7935167
   ]
 ];
