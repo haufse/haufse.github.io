699ee3e776868f801e122129c6f54badae759901
haufse <erik@haufs.de>
2025-07-12 23:40:05 +0200

    try colored map, may break

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -2,41 +2,92 @@ var addressPoints = [
   [
     "Auf dem H\u00fcgel 20, 53121 Bonn, Germany",
     50.7311926,
-    7.0707041
+    7.0707041,
+    "talk"
   ],
   [
     "405 Rue Sainte-Catherine Est, Montr\u00e9al, QC H2L 2C4",
     45.513713,
-    -73.5617177
+    -73.5617177,
+    "visit / position"
   ],
   [
     "Ludwigstra\u00dfe 23, 35390 Gie\u00dfen, Germany",
     50.5804148,
-    8.6771688
+    8.6771688,
+    "talk"
   ],
   [
     "Vogelpothsweg 87, 44227 Dortmund, Germany",
     51.4920599,
-    7.4153013
+    7.4153013,
+    "talk"
   ],
   [
     "Ruhr University Bochum, Universit\u00e4tsstra\u00dfe 150, 44801 Bochum, Germany",
     51.4441576,
-    7.2609654
+    7.2609654,
+    "visit / position"
   ],
   [
     "700 University Ave, Toronto, ON M5G 1Z5",
     43.6591104,
-    -79.3912066
+    -79.3912066,
+    "visit / position"
   ],
   [
     "University of North Carolina at Chapel Hill, Chapel Hill, NC, USA",
     35.9050353,
-    -79.0477533
+    -79.0477533,
+    "conference / workshop"
   ],
   [
     "Universit\u00e4tsstra\u00dfe 1, 40225 D\u00fcsseldorf, Germany",
     51.1907732,
-    6.7935167
+    6.7935167,
+    "talk"
   ]
 ];
+
+// var addressPoints = [
+//   [
+//     "Auf dem H\u00fcgel 20, 53121 Bonn, Germany",
+//     50.7311926,
+//     7.0707041
+//   ],
+//   [
+//     "405 Rue Sainte-Catherine Est, Montr\u00e9al, QC H2L 2C4",
+//     45.513713,
+//     -73.5617177
+//   ],
+//   [
+//     "Ludwigstra\u00dfe 23, 35390 Gie\u00dfen, Germany",
+//     50.5804148,
+//     8.6771688
+//   ],
+//   [
+//     "Vogelpothsweg 87, 44227 Dortmund, Germany",
+//     51.4920599,
+//     7.4153013
+//   ],
+//   [
+//     "Ruhr University Bochum, Universit\u00e4tsstra\u00dfe 150, 44801 Bochum, Germany",
+//     51.4441576,
+//     7.2609654
+//   ],
+//   [
+//     "700 University Ave, Toronto, ON M5G 1Z5",
+//     43.6591104,
+//     -79.3912066
+//   ],
+//   [
+//     "University of North Carolina at Chapel Hill, Chapel Hill, NC, USA",
+//     35.9050353,
+//     -79.0477533
+//   ],
+//   [
+//     "Universit\u00e4tsstra\u00dfe 1, 40225 D\u00fcsseldorf, Germany",
+//     51.1907732,
+//     6.7935167
+//   ]
+// ];
